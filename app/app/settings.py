424c7b98938c0bf63699bef952de3733d843d99c
dev-carki <94.nogari@kakao.com>
2025-08-24 22:46:27 +0900

    [add] token api added

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken', # 인증토큰
     'drf_spectacular',
     'core',
     'user',
